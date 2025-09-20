ba497f681057e77c721800775bb84e435347cb29
Vovaks <rjabkovvladimir@gmail.com>
2025-09-20 17:46:43 +0300

    added swa

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://azure.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello Azure!
         </a>
       </header>
     </div>
